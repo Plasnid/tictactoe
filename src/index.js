89a8a5ddcb8fec2d427c6aef5efa9834ec323792
Plasnid <stuart@sackler.ca>
2020-02-10 13:45:10 -0500

    isNull not being used

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { isNullOrUndefined, isNull } from 'util';
+//import { isNullOrUndefined, isNull } from 'util';
 
 function Square(props) {
     return (
